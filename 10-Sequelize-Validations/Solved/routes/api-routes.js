b3d23513c723ec85f8db03e58ebffeddf38a864b
Aliagho <ali.bahrami633@yahoo.com>
2020-05-07 20:57:02 +1000

    added deconstruction feature to all the codes

--- 10-Sequelize-Validations/Solved/routes/api-routes.js
+++ 10-Sequelize-Validations/Solved/routes/api-routes.js
@@ -10,69 +10,62 @@ var db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = (app) => {
 
   // GET route for getting all of the todos
-  app.get("/api/todos", function(req, res) {
+  app.get("/api/todos", (req, res) => {
     // findAll returns all entries for a table when used with no options
-    db.Todo.findAll({}).then(function(dbTodo) {
+    db.Todo.findAll({}).then((dbTodo) => {
       // We have access to the todos as an argument inside of the callback function
       res.json(dbTodo);
     });
   });
 
   // POST route for saving a new todo
-  app.post("/api/todos", function(req, res) {
+  app.post("/api/todos", (req, res) => {
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
-    // and complete property (req.body)
+    // and complete property
     db.Todo.create({
       text: req.body.text,
       complete: req.body.complete
-    }).then(function(dbTodo) {
+    }).then((dbTodo) => {
       // We have access to the new todo as an argument inside of the callback function
       res.json(dbTodo);
-    })
-      .catch(function(err) {
-      // Whenever a validation or flag fails, an error is thrown
-      // We can "catch" the error to prevent it from being "thrown", which could crash our node app
-        res.json(err);
-      });
+    });
   });
 
   // DELETE route for deleting todos. We can get the id of the todo to be deleted from
   // req.params.id
-  app.delete("/api/todos/:id", function(req, res) {
+  app.delete("/api/todos/:id", (req, res) => {
     // We just have to specify which todo we want to destroy with "where"
+    const { param: { id } } = req; // deconstruction
     db.Todo.destroy({
       where: {
-        id: req.params.id
+        id  // id: req.params.id   // deconstruction
       }
-    }).then(function(dbTodo) {
+    }).then((dbTodo) => {
       res.json(dbTodo);
     });
 
   });
 
   // PUT route for updating todos. We can get the updated todo data from req.body
-  app.put("/api/todos", function(req, res) {
-
+  app.put("/api/todos", (req, res) => {
     // Update takes in an object describing the properties we want to update, and
     // we use where to describe which objects we want to update
+    const { body: { text, complete, id } } = req; // deconstruction
+
     db.Todo.update({
-      text: req.body.text,
-      complete: req.body.complete
+      text,  //  text: req.body.text, // deconstruction
+      complete  //  complete: req.body.complete // deconstruction
     }, {
       where: {
-        id: req.body.id
+        id  //  id: req.body.id // deconstruction
       }
-    }).then(function(dbTodo) {
+    }).then((dbTodo) => {
       res.json(dbTodo);
-    })
-      .catch(function(err) {
-      // Whenever a validation or flag fails, an error is thrown
-      // We can "catch" the error to prevent it from being "thrown", which could crash our node app
-        res.json(err);
-      });
+    });
   });
+
 };
