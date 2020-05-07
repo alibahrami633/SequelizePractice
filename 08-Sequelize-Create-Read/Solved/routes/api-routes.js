ead607372d95336568c6ab88c0f023c89a757526
Aliagho <ali.bahrami633@yahoo.com>
2020-05-07 20:05:41 +1000

    added get and post

--- 08-Sequelize-Create-Read/Solved/routes/api-routes.js
+++ 08-Sequelize-Create-Read/Solved/routes/api-routes.js
@@ -10,27 +10,29 @@ var db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // GET route for getting all of the todos
-  app.get("/api/todos", function(req, res) {
+  app.get("/api/todos", function (req, res) {
     // findAll returns all entries for a table when used with no options
-    db.Todo.findAll({}).then(function(dbTodo) {
+    db.Todo.findAll({}).then(function (dbTodo) {
       // We have access to the todos as an argument inside of the callback function
       res.json(dbTodo);
     });
   });
 
   // POST route for saving a new todo
-  app.post("/api/todos", function(req, res) {
+  app.post("/api/todos", function (req, res) {
     console.log(req.body);
+
+    const { body: { text, complete } } = req; // deconstruct
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
     // and complete property (req.body)
     db.Todo.create({
-      text: req.body.text,
-      complete: req.body.complete
-    }).then(function(dbTodo) {
+      text,  //text: req.body.text, // deconstructed
+      complete  //complete: req.body.complete // deconstructed
+    }).then(function (dbTodo) {
       // We have access to the new todo as an argument inside of the callback function
       res.json(dbTodo);
     });
@@ -38,12 +40,12 @@ module.exports = function(app) {
 
   // DELETE route for deleting todos. We can get the id of the todo we want to delete from
   // req.params.id
-  app.delete("/api/todos/:id", function(req, res) {
+  app.delete("/api/todos/:id", function (req, res) {
 
   });
 
   // PUT route for updating todos. We can get the updated todo from req.body
-  app.put("/api/todos", function(req, res) {
+  app.put("/api/todos", function (req, res) {
 
   });
 };
