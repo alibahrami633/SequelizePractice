0013728cc2b8039f982ec201377cea7e316792f5
Aliagho <ali.bahrami633@yahoo.com>
2020-05-09 10:55:55 +1000

    practice 11 done

--- 11-Post-Model/Solved/models/post.js
+++ 11-Post-Model/Solved/models/post.js
@@ -1,10 +1,10 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var Post = sequelize.define("Post", {
     title: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1]
+        len: [1, 160]
       }
     },
     body: {
