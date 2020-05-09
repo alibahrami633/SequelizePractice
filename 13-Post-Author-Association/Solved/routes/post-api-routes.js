36e7bca312d2009f1060eeada9d8453515e8ded4
Aliagho <ali.bahrami633@yahoo.com>
2020-05-09 13:09:00 +1000

    deconstructed some of the parameters

--- 13-Post-Author-Association/Solved/routes/post-api-routes.js
+++ 13-Post-Author-Association/Solved/routes/post-api-routes.js
@@ -10,61 +10,63 @@ var db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // GET route for getting all of the posts
-  app.get("/api/posts", function(req, res) {
+  app.get("/api/posts", function (req, res) {
     var query = {};
     if (req.query.author_id) {
       query.AuthorId = req.query.author_id;
     }
     db.Post.findAll({
       where: query
-    }).then(function(dbPost) {
+    }).then(function (dbPost) {
       res.json(dbPost);
     });
   });
 
   // Get route for retrieving a single post
-  app.get("/api/posts/:id", function(req, res) {
+  app.get("/api/posts/:id", function (req, res) {
     db.Post.findOne({
       where: {
         id: req.params.id
       }
-    }).then(function(dbPost) {
+    }).then(function (dbPost) {
       console.log(dbPost);
       res.json(dbPost);
     });
   });
 
   // POST route for saving a new post
-  app.post("/api/posts", function(req, res) {
-    db.Post.create(req.body).then(function(dbPost) {
+  app.post("/api/posts", function (req, res) {
+    db.Post.create(req.body).then(function (dbPost) {
       res.json(dbPost);
     });
   });
 
   // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function(req, res) {
+  app.delete("/api/posts/:id", function (req, res) {
+    const { params: { id } } = req; // deconstruction
+
     db.Post.destroy({
       where: {
-        id: req.params.id
+        id  // id: req.params.id  // deconstruction
       }
-    }).then(function(dbPost) {
+    }).then(function (dbPost) {
       res.json(dbPost);
     });
   });
 
   // PUT route for updating posts
-  app.put("/api/posts", function(req, res) {
+  app.put("/api/posts", function (req, res) {
     db.Post.update(
       req.body,
       {
         where: {
           id: req.body.id
         }
-      }).then(function(dbPost) {
-      res.json(dbPost);
-    });
+      }).then(function (dbPost) {
+        res.json(dbPost);
+      });
   });
 };
